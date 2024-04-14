637e6753265a595f104dde1aecfc076ab80379fa
Emmanuel Montoya Lopez <emmanuelprogramador24@gmail.com>
2024-04-14 15:10:35 -0500

    Raiz del proyecto

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include('./db/conexion.php');
+date_default_timezone_set('America/Bogota');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,12 +10,30 @@
 include('./inc/head.php')
 ?>
 
-<body class="w-full bg-dark-primary h-screen scroll-container">
+<body class="w-full bg-dark-primary h-screen scroll-container box-border">
   <?php
 
   if (is_file("./vistas/" . $_GET['vista'] . ".php") && $_GET['vista'] != "404") {
     include('./inc/navbar.php');
     include("./vistas/" . $_GET['vista'] . ".php");
+    include('./inc/script.php');
+  } else if (is_file("./vistasUser/" . $_GET['vista'] . ".php") && $_GET['vista'] != "404") {
+    // Restringir accesso
+    if ((!isset($_SESSION['id']) || $_SESSION['id']) == "" || (!isset($_SESSION['usuario'])) || $_SESSION['usuario'] == "") {
+      require_once("./vistasUser/logout.php");
+      exit();
+    }
+    include('./inc/navbarUser.php');
+    include("./vistasUser/" . $_GET['vista'] . ".php");
+    include('./inc/script.php');
+  } else if (is_file("./vistasAdministrador/" . $_GET['vista'] . ".php") && $_GET['vista'] != "404") {
+    // // Restringir accesso
+    // if ((!isset($_SESSION['id']) || $_SESSION['id']) == "" || (!isset($_SESSION['usuario'])) || $_SESSION['usuario'] == "") {
+    //   require_once("./vistasUser/logout.php");
+    //   exit();
+    // }
+    include('./inc/navbarAdmin.php');
+    include("./vistasAdministrador/" . $_GET['vista'] . ".php");
   } else {
     include("./vistas/404.php");
   }
